107f8f181467ef6633072b58e1aab3e0db6987b4
Carlos Garcia Ortiz <yo@carlosgo.me>
2019-03-21 22:19:05 +0100

    Adding host

--- bin/start.js
+++ bin/start.js
@@ -6,19 +6,21 @@ const startRailsAndTestCypress = require('..')
 
 let url;
 let cypressPort;
+let host;
 
-if (args.length === 2) {
+if (args.length === 3) {
   url = args[0];
   cypressPort = args[1];
+  host = args[2];
 } else {
-  la(args.lenght, 'expect: <url> <cypress-port>')
+  la(args.lenght, 'expect: <url> <cypress-port> <host>')
 }
 
 console.log(`starting rails server`)
 console.log(`and when url "${url}" is responding`)
 console.log(`running cypress on port "${cypressPort}"`)
 
-startRailsAndTestCypress({ url, cypressPort }).catch(e => {
+startRailsAndTestCypress({ url, cypressPort, host }).catch(e => {
   console.error(e)
   process.exit(1)
 })
